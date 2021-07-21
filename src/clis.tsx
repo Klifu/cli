a0dddec1cfb064924bd0cfade168df61e35416cd
Souvik <souvikde.ns@gmail.com>
2021-07-21 13:09:31 +0530

    feat: fixed issue with klifu core library

--- src/clis.tsx
+++ src/clis.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {FunctionComponent} from 'react';
 import { render, Text } from 'ink';
+import {PokemonService} from '@klifu/core';
 
-const App = () => {
-	return <Text>Hello World</Text>
+const App: FunctionComponent<{pokemonService: PokemonService}> = ({pokemonService}) => {
+	console.log(pokemonService.pokemons);
+	
+	return <Text>
+		{pokemonService.pokemons.map(el => <Text>{el.name}</Text>)}
+	</Text>
 }
 
-render(<App />)
+const loader = async (Component: FunctionComponent<{pokemonService: PokemonService}>) => {
+	const pokemonService = await PokemonService.load();
+
+	const Comp = () => {
+		return <Component pokemonService={pokemonService} />
+	}
+
+	return Comp
+}
+
+(async () => {
+	let Comp = await loader(App);
+	render(<Comp />)
+})()
