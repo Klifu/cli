5d849dccbbe6c611f4766ccef366b9f031802533
Souvik <souvikde.ns@gmail.com>
2021-07-27 21:01:30 +0530

    fix: added promise handle to loader hook

--- src/hooks/loader.tsx
+++ src/hooks/loader.tsx
@@ -4,15 +4,22 @@ import { PokemonService } from '@klifu/core';
 export const usePokemonService = () => {
 	const [loading, setLoading] = useState<boolean>(true);
 	const [pokemonService, setPokemonService] = useState<PokemonService | undefined>();
+	const [error, setError] = useState(null);
 	const loadService = async () => {
-		let pkService: PokemonService = await PokemonService.load();
-		setPokemonService(pkService);
-		setLoading(false);
+		try {
+			let pkService: PokemonService = await PokemonService.load();
+			setPokemonService(pkService);
+			setLoading(false);
+		} catch (error) {
+			setError(error);
+			setLoading(false);
+		}
+
 	}
 
 	useEffect(() => {
 		loadService();
 	}, [])
 
-	return { loading, pokemonService };
+	return { loading, pokemonService, error };
 }
